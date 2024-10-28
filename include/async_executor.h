6164faf653b48195f2b5547d0e647229c63b64c5
flychensc <flychensc@163.com>
2024-10-28 21:02:34 +0800

    added async executor

---
+++ include/async_executor.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <functional>
+#include <future>
+#include <memory>
+#include <mutex>
+#include <unordered_map>
+
+#include "pack.h"
+#include "util.h"
+#include "loquat/include/listener.h"
+
+namespace mango
+{
+    struct AsyncContext
+    {
+        std::string session_id;
+        std::future<std::vector<loquat::Byte>> future;
+    };
+
+    class CycleTimer : public loquat::ReadWritable
+    {
+    public:
+        CycleTimer();
+        ~CycleTimer();
+
+        int TimerFd() { return timer_fd_; }
+
+        virtual void OnRead(int sock_fd) override final;
+        void OnWrite(int sock_fd) override final {}
+
+        void registerTimeoutCallback(std::function<void(void)> callback) { timeoutCallback_ = callback; }
+
+    private:
+        int timer_fd_;
+        std::function<void(void)> timeoutCallback_;
+    };
+
+    class AsyncExecutor : public loquat::Connection
+    {
+    public:
+        AsyncExecutor(int listen_fd);
+
+        void OnClose(int sock_fd) override final;
+
+        void registerCloseHandler(std::function<void(int)> callback);
+
+    protected:
+        void OnRecv(const std::vector<loquat::Byte> &data) override final;
+
+    private:
+        void pollRpcCallStatus();
+
+        std::function<void(int)> close_callback_;
+
+        RecvState recv_state_;
+        std::string last_recv_sess_id_;
+
+        CycleTimer cycle_timer_;
+        std::list<AsyncContext> asyncContexts_;
+    };
+
+    class AsyncExecutorService : public loquat::Listener
+    {
+    public:
+        static const int kMaxConnections = 10;
+
+        AsyncExecutorService(const std::string &unix_path);
+        AsyncExecutorService(const std::string &address, int port);
+
+        void OnAccept(int listen_sock) override final;
+
+        void insertExecutor(int sock_fd, std::shared_ptr<AsyncExecutor> ptr);
+        void removeExecutor(int sock_fd);
+
+    private:
+        std::mutex mutex_;
+        std::unordered_map<int, std::shared_ptr<AsyncExecutor>> executors_;
+    };
+}
---
+++ src/async_executor.cc
@@ -0,0 +1,184 @@
+#include "async_executor.h"
+#include "message_creator.h"
+#include <spdlog/spdlog.h>
+#include <sys/timerfd.h>
+#include "loquat/include/epoll.h"
+
+namespace mango
+{
+    CycleTimer::CycleTimer()
+    {
+        timer_fd_ = timerfd_create(CLOCK_MONOTONIC, 0);
+        if (timer_fd_ == -1)
+        {
+            throw std::runtime_error("AsyncExecutor:timerfd_create");
+        }
+
+        struct itimerspec new_value;
+        new_value.it_value.tv_sec = 1;
+        new_value.it_value.tv_nsec = 0;
+        new_value.it_interval.tv_sec = 0;
+        new_value.it_interval.tv_nsec = 50000000;
+
+        timerfd_settime(timer_fd_, 0, &new_value, nullptr);
+    }
+
+    CycleTimer::~CycleTimer()
+    {
+        close(timer_fd_);
+    }
+
+    void CycleTimer::OnRead(int sock_fd)
+    {
+        if (timeoutCallback_)
+        {
+            timeoutCallback_();
+        }
+    }
+
+    AsyncExecutor::AsyncExecutor(int listen_fd) : loquat::Connection(Stream::Type::Framed, listen_fd), recv_state_(RecvState::RECV_ID_LENGTH)
+    {
+        SetBytesNeeded(1);
+        cycle_timer_.registerTimeoutCallback([this]()
+                                             { pollRpcCallStatus(); });
+    }
+
+    void AsyncExecutor::OnRecv(const std::vector<loquat::Byte> &data)
+    {
+        switch (recv_state_)
+        {
+        case RecvState::RECV_ID_LENGTH:
+            spdlog::debug("RECV_ID_LENGTH");
+            // action
+            {
+                SetBytesNeeded(data[0]);
+            }
+            // next state
+            recv_state_ = RecvState::RECV_ID_VALUE;
+            break;
+
+        case RecvState::RECV_ID_VALUE:
+            spdlog::debug("RECV_ID_VALUE");
+            // action
+            {
+                last_recv_sess_id_.assign(data.begin(), data.end());
+                SetBytesNeeded(2);
+
+                spdlog::debug("last_recv_sess_id_ {}", last_recv_sess_id_);
+            }
+            // next state
+            recv_state_ = RecvState::RECV_MSG_LENGTH;
+            break;
+
+        case RecvState::RECV_MSG_LENGTH:
+            spdlog::debug("RECV_MSG_LENGTH");
+            // action
+            {
+                u_int16_t length = (((data[0] & 0xFFU) << 8) | (data[1] & 0xFFU));
+                SetBytesNeeded(length);
+
+                spdlog::debug("Message length {}", length);
+            }
+            // next state
+            recv_state_ = RecvState::RECV_MSG_VALUE;
+            break;
+
+        case RecvState::RECV_MSG_VALUE:
+            spdlog::debug("RECV_MSG_VALUE");
+            // action
+            {
+                // Deserialize Message
+                auto message = MessageCreator::Deserialize(data);
+                spdlog::debug("message type {}", message->Type);
+
+                // Execute
+                AsyncContext asyncCtx;
+                asyncCtx.future = std::async(std::launch::async, [this, message]
+                                             {
+                                                Context context;
+                                                message->OnCall(context);
+                                                context.is_completed = true;
+                                                return context.reply; });
+
+                SetBytesNeeded(1);
+            }
+            // next state
+            recv_state_ = RecvState::RECV_ID_LENGTH;
+            break;
+        }
+    }
+
+    void AsyncExecutor::pollRpcCallStatus()
+    {
+        for (auto &context : asyncContexts_)
+        {
+            if (context.future.wait_for(std::chrono::seconds(0)) == std::future_status::ready)
+            {
+                auto reply = context.future.get();
+                if (reply.size() > 0)
+                {
+                    // Enqueue Header
+                    Enqueue(packHeader(context.session_id, reply.size()));
+                    // Enqueue Reply
+                    Enqueue(reply);
+
+                    spdlog::debug("async reply {} bytes", reply.size());
+                }
+                else
+                {
+                    spdlog::debug("nothing to async reply");
+                }
+            }
+        }
+    }
+
+    void AsyncExecutor::OnClose(int sock_fd)
+    {
+        spdlog::debug("Executor {} closed", sock_fd);
+
+        if (close_callback_)
+        {
+            close_callback_(sock_fd);
+        }
+    }
+
+    void AsyncExecutor::registerCloseHandler(std::function<void(int)> callback)
+    {
+        close_callback_ = callback;
+    }
+
+    AsyncExecutorService::AsyncExecutorService(const std::string &unix_path) : loquat::Listener(determineDomain(unix_path), kMaxConnections)
+    {
+        Listen(unix_path);
+    }
+
+    AsyncExecutorService::AsyncExecutorService(const std::string &address, int port) : loquat::Listener(determineDomain(address), kMaxConnections)
+    {
+        Listen(address, port);
+    }
+
+    void AsyncExecutorService::OnAccept(int listen_sock)
+    {
+        auto executor_ptr = std::make_shared<AsyncExecutor>(listen_sock);
+
+        spdlog::debug("Accept a executor {}", executor_ptr->Sock());
+
+        executor_ptr->registerCloseHandler([this](int sock_fd)
+                                           { removeExecutor(sock_fd); });
+
+        loquat::Epoll::GetInstance().Join(executor_ptr->Sock(), executor_ptr);
+        insertExecutor(executor_ptr->Sock(), executor_ptr);
+    }
+
+    void AsyncExecutorService::insertExecutor(int sock_fd, std::shared_ptr<AsyncExecutor> ptr)
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        executors_.insert({sock_fd, ptr});
+    }
+
+    void AsyncExecutorService::removeExecutor(int sock_fd)
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        executors_.erase(sock_fd);
+    }
+}
